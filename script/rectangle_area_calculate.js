f0eef8dc0fb19962638e48569da0e9304eeb60ae
gfxrobiul <mrhrobiul008877@gmail.com>
2024-02-13 16:26:01 +0600

    added reactangle card design complete

--- script/rectangle_area_calculate.js
+++ script/rectangle_area_calculate.js
@@ -1,3 +1,23 @@
 function calculateRectangleArea(){
+    //get length the of reactangle
+    const lengthInput= document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    // get width of the reactagle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+    // calculate reactangle area 
+    const area =length * width;
+    console.log('area of the reactangle area:', area);
+
+    // display reactangle area 
+    const reactagleAreaSpan = document.getElementById('rectangle-area');
+    reactagleAreaSpan.innerText = area;
+
 
 }
